8194df614905d462a64525ba6d434ced9c9322aa
74opt <potatomatt0505@gmail.com>
2022-03-07 12:32:59 -0500

    k

--- src/main/java/F7/ui/ServerMenu.java
+++ src/main/java/F7/ui/ServerMenu.java
@@ -11,6 +11,66 @@ public class ServerMenu {
         network = new Network(Network.MAIN_PORT, name);
     }
 
+    private static void initialDraw() throws Exception {
+        Lanterna.clear();
+
+        // Setting color
+        Lanterna.print(0, 0, "^G");
+
+        // Top line
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 211; i++) {
+                    switch (i) {
+                        case 6, 205 -> Lanterna.print(i, 0, "╦");
+                        default -> Lanterna.print(i, 0, "═");
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        // Second line
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 211; i++) {
+                    switch (i) {
+                        case 6, 205 -> Lanterna.print(i, 2, "╦");
+                        default -> Lanterna.print(i, 2, "═");
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+        
+        // Bottom line
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < 211; i++) {
+                    switch (i) {
+                        case 6, 205 -> Lanterna.print(i, 61, "╩");
+                        default -> Lanterna.print(i, 61, "═");
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        // First vertical line
+
+
+        // Second vertical line
+
+
+        // Third vertical line
+
+
+        // Fourth vertical line
+    }
+
     // TODO: top priority, design a server menu (whiteboard time)
     public static void menu() {
         // List this info: server name, latency, player count
